007a01165e7b64c3ca0726360c4cacef98cb84b2
Kirill Biller <biller.k@yandex.ru>
2023-06-07 23:01:11 +0300

    added hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  # config.hosts << 'scrumpoker-production-c4d6.up.railway.app'
+  config.hosts << "web-production-3554.up.railway.app"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
